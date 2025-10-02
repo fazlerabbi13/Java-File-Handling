63ac9b67b81ef71ca0f942a53e427c50b916653d
fazlerabbi13 <mohammad0132794@gmail.com>
2025-10-02 22:03:18 +0600

    creating a file

---
+++ CreateFile.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.IOException;
+
+public class CreateFile {
+    public static void main(String[] args) {
+        try {
+            File file = new File("myFile.txt");
+            
+            if(file.createNewFile()){
+                System.out.println("File is created" + file.getName());
+            }else{
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("error");
+            e.printStackTrace();
+        }
+    }
+}
