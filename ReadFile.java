6186d03e62389d9b78c58093f8fea54df6f55a7f
fazlerabbi13 <mohammad0132794@gmail.com>
2025-10-02 22:18:48 +0600

    traverse file data.

---
+++ ReadFile.java
@@ -0,0 +1,23 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadFile {
+    public static void main(String[] args) throws FileNotFoundException {
+        try{
+            File file = new File("myFile");
+            Scanner Reader = new Scanner(file);
+
+            while(Reader.hasNextLine()){
+                String data = Reader.nextLine();
+                System.out.println(data);
+            }
+            Reader.close();
+        }
+        catch(FileNotFoundException e){
+            System.out.println("error");
+            e.printStackTrace();
+        }
+    }
+}
