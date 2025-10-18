2e54667c144c1bee4636d4e48d7dd92b68d9d967
fazlerabbi13 <mohammad0132794@gmail.com>
2025-10-18 22:41:13 +0600

    Open a file reader and Create a character array and CharBuffer.

---
+++ ReaderClass/Reader.java
@@ -0,0 +1,19 @@
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.CharBuffer;
+
+
+public class Reader {
+    public static void main(String[] args) 
+    throws IOException{
+        FileReader r = new FileReader("myFile.txt");
+        PrintStream out = System.out;
+
+        char[] buffer = new char[10];
+        CharBuffer charBuffer = CharBuffer.wrap(buffer);
+
+        
+    }
+}
