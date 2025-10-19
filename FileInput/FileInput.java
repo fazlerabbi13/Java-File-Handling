aeab06671f585b174ec5eacee4152ac32fd17b9f
fazlerabbi13 <mohammad0132794@gmail.com>
2025-10-19 21:16:24 +0600

    file read using fileInputStream.

---
+++ FileInput/FileInput.java
@@ -0,0 +1,28 @@
+package FileInput;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileInput {
+    public static void main(String[] args) {
+        try {
+            FileInputStream fi = new FileInputStream("myFile.txt");
+            
+
+            // file read character by character
+            // int ch;
+            // while((ch = fi.read()) != -1){
+            //     System.out.print((char)ch);
+            // }
+
+            fi.close();
+        
+        
+        } catch (FileNotFoundException e) {
+            System.out.println("file not found.");
+        }catch(IOException e){
+            System.out.println("an error occured");
+        }
+    }
+}
